528b18838a01f4657339126f58819cdcf1c79357
Denis Serkhel <ripvanfish27@gmail.com>
2021-11-01 13:34:10 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 
+function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+  // Change code below this line
+const totalPrice = (orderedQuantity * pricePerDroid) + deliveryFee;
+    const message = `You ordered droids worth ${totalPrice} credits. Delivery ${deliveryFee} is included in total price.`;
+
+  // Change code above this line
+  return message;
+}
+
+// makeOrderMessage(4, 500, 100);
+
+console.log(makeOrderMessage);
 
