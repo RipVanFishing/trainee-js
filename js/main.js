b3669666fe72eb13d081bc7d908b4b003ff0a963
Denis Serkhel <ripvanfish27@gmail.com>
2022-02-06 13:15:37 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,38 +1,14 @@
-// Задача № 2
-
-// Given the triangle of consecutive odd numbers:
-
-//  1
-//           3     5
-//        7     9    11
-//    13    15    17    19
-// 21    23    25    27    29
-// ...
-
-// Calculate the sum of the numbers in the nth row of this triangle(starting at index 1) e.g.: (Input-- > Output)
-
-// 1 -->  1
-// 2 --> 3 + 5 = 8
-
-// function rowSumOddNumbers(n) {
-// 	return Math.pow(n, 3)
-// }
-
-// console.log(rowSumOddNumbers(6))
-
-let sum = 0;
-
-function calculateTotal(number) {
- // Change code below this line
-for( let i = 0; i <= number; i += 1) {
-	
-	sum += i;
-	
+function filterArray(numbers, value) {
+   // Change code below this line
+  const newArr = [];
+for (const num of numbers) {
+  if( num > value ) {
+    newArr.push(num) 
+  }
 }
-	return sum;
+return newArr;
+
   // Change code above this line
 }
 
-// console.log(calculateTotal(24));
-
-console.log(calculateTotal(10));
+console.log(filterArray([12, 24, 8, 41, 76], 9));
